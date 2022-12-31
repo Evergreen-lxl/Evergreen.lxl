f20005aa1209c11b499b98fa67c742619d2d3f91
sammyette <torchedsammy@gmail.com>
2022-12-31 11:35:53 -0400

    feat: add alias for all highlight groups and warn about missing groups

--- style.lua
+++ style.lua
@@ -1,30 +1,70 @@
+local core = require 'core'
 local style = require 'core.style'
+local missingCount = 0
+local totalCount = 0
 
 local function addAlias(to, from)
 	if not style.syntax[to] then
+		missingCount = missingCount + 1
 		style.syntax[to] = style.syntax[from]
 	end
 end
 
-addAlias('boolean', 'literal')
+local altMap = {
+	literal = {
+		'boolean',
+		'constant',
+		'float',
+		'number',
+		'label'
+	},
+	constant = {'constant.builtin'},
+	keyword = {
+		'attribute',
+		'conditional',
+		'define',
+		'exception',
+		'include',
+		'keyword.function',
+		'keyword.return',
+		'namespace',
+		'preproc',
+		'repeat',
+		'type.qualifier'
+	},
+	keyword2 = {
+		'type', -- this is suitable i think?
+		'type.builtin',
+		'variable.builtin',
+		'type.definition',
+	},
+	operator = {
+		'conditional.ternary',
+		'keyword.operator'
+	},
+	['function'] = {
+		'function.call',
+		'method',
+	},
+	method = {'method.call'},
+	normal = {
+		'field',
+		'punctuation.delimiter',
+		'punctuation.brackets',
+		'variable'
+	}
+}
 
-addAlias('constant', 'literal')
-addAlias('constant.builtin', 'literal')
-addAlias('conditional', 'keyword')
-addAlias('conditional.ternary', 'operator')
+for from, tos in pairs(altMap) do
+	for _, to in ipairs(tos) do
+		addAlias(to, from)
+	end
 
-addAlias('define', 'keyword')
+	totalCount = totalCount + #tos
+end
 
-addAlias('exception', 'keyword')
-
-addAlias('field', 'normal')
-addAlias('function.call', 'function')
-addAlias('float', 'literal')
-
-addAlias('number', 'literal')
-
-addAlias('keyword.function', 'keyword')
-addAlias('keyword.operator', 'operator')
-
-addAlias('method', 'function')
-addAlias('method.call', 'method') -- if user doesnt have custom method highlight it will fallback to function
+core.add_thread(function()
+	if missingCount > 0 then
+		core.warn(string.format('Missing %d/%d style variables for Evergreen syntax highlighting. To get the full experience, you can set these separately.', missingCount, totalCount))
+	end
+end)
