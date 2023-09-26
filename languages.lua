78b5c01d52596e1a5f7c9a203ef2c1a5c0caffd8
ShalokShalom <shalokshalom@protonmail.ch>
2023-09-26 22:06:25 +0000

    add a comma
    adding comma

--- languages.lua
+++ languages.lua
@@ -38,7 +38,7 @@ M.exts = {
 	go = 'https://github.com/tree-sitter/tree-sitter-go',
 	gomod = 'https://github.com/camdencheek/tree-sitter-go-mod',
 	javascript = 'https://github.com/tree-sitter/tree-sitter-javascript',
-	julia = 'https://github.com/tree-sitter/tree-sitter-julia'
+	julia = 'https://github.com/tree-sitter/tree-sitter-julia',
 	lua = 'https://github.com/MunifTanjim/tree-sitter-lua',
 	rust = 'https://github.com/tree-sitter/tree-sitter-rust'
 }
