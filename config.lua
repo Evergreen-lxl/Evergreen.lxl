300df49926783d91c863cff1874efee387b4633e
Martino Ferrari <martinogiordano.ferrari@iter.org>
2023-10-27 08:47:45 +0200

    refactor!: refactoring of installation and configuration of grammars in a more rational way
    BREAKING CHANGE: `addGrammar` is now moved into the module `config`, `installer` module is also back

--- config.lua
+++ config.lua
@@ -1,14 +1,20 @@
 local util = require 'plugins.evergreen.util'
+local installer = require 'plugins.evergreen.installer'
+
 local home = HOME or os.getenv 'HOME'
 
 local M = {
 	dataDir = PLATFORM ~= 'Windows'
-	and ('~/.local/share/evergreen'):gsub('~', home)
-	or string.format('%s\\evergreen', os.getenv 'APPDATA')
+			and ('~/.local/share/evergreen'):gsub('~', home)
+			or string.format('%s\\evergreen', os.getenv 'APPDATA')
 }
 
-M.parserLocation = util.join {M.dataDir, 'parsers'}
-M.queryLocation = util.join {M.dataDir, 'queries'}
+M.parserLocation = util.join { M.dataDir, 'parsers' }
+M.queryLocation = util.join { M.dataDir, 'queries' }
+
+-- add grammar
+function M.addGrammar(options)
+	installer.addGrammar(options, M)
+end
 
 return M
-
