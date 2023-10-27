bbe676e94b8d65c96b182873aa6c3093ee502db4
Martino Ferrari <martinogiordano.ferrari@iter.org>
2023-10-27 08:31:19 +0200

    refactor!: refactored code and used camel case for all functions
    BREAKING CHANGE: `add_grammar` become `addGrammar`

--- languages.lua
+++ languages.lua
@@ -5,21 +5,24 @@ local config = require 'plugins.evergreen.config'
 
 local M = {}
 
-M.grammars = {}
+-- map grammars to file extensions
 M.extensionMappings = {}
+-- map grammars to specific file names
 M.filenameMappings = {}
 
-
+-- check if file exists
 local function exists(path)
 	local f = system.get_file_info(path)
 	if f ~= nil then return f.type == "file" else return false end
 end
 
+-- check if path is a directory or not
 local function isDir(path)
 	local f = system.get_file_info(path)
 	if f ~= nil then return f.type == "dir" else return false end
 end
 
+-- execute proecess
 local function exec(cmd, opts)
 	local proc = process.start(cmd, opts or {})
 	if proc then
@@ -29,6 +32,23 @@ local function exec(cmd, opts)
 		return (proc:read_stdout() or '<no stdout>\n') .. (proc:read_stderr() or '<no stderr>'), proc:returncode()
 	end
 	return nil
+end
+
+-- remove directory
+local function rmDir(path)
+	exec(PLATFORM == 'Windows' and
+		{ 'cmd', '/c', 'rmdir ' .. path } or
+		{ 'sh', '-c', 'rm -rf ' .. path })
+end
+
+local function copyQueries(source, dest)
+	local out, exitCode = exec(PLATFORM == 'Windows' and
+		{ 'cmd', '/c', 'cp ' .. util.join { source, '*.scm' } .. ' ' .. dest } or
+		{ 'sh', '-c', 'cp ' .. util.join { source, '*.scm' } .. ' ' .. dest })
+	if exitCode ~= 0 then
+		core.error('[Evergreen] An error occured while copying queries from ' .. source .. ' to ' .. dest .. ' \n' .. out)
+	end
+	return exitCode == 0
 end
 
 local function compileParser(lang, path, dest)
@@ -47,6 +67,19 @@ local function compileParser(lang, path, dest)
 	end
 end
 
+local function mapGrammar(options)
+	if options.extensions ~= nil then
+		for ext in options.extensions:gmatch('[^,]+') do
+			M.extensionMappings[ext] = options.lang
+		end
+	end
+	if options.filename ~= nil then
+		for name in options.filename:gmatch('[^,]+') do
+			M.filenameMappings[name] = options.lang
+		end
+	end
+end
+
 local function installGrammar(options)
 	local path = util.join { config.parserLocation, options.lang }
 	if isDir(options.path) then
@@ -58,26 +91,11 @@ local function installGrammar(options)
 			if options.queries ~= nil then
 				queries = options.queries
 			end
-			local out, exitCode = exec(PLATFORM == 'Windows' and
-				{ 'cmd', '/c', 'cp ' .. util.join { queries, '*.scm' } .. ' ' .. queryPath } or
-				{ 'sh', '-c', 'cp ' .. util.join { queries, '*.scm' } .. ' ' .. queryPath }, { cwd = options.path })
-			if exitCode ~= 0 then
-				core.error('[Evergreen] An error occured while copying ' .. options.lang .. 'queries \n' .. out)
-				return false
-			else
+			if copyQueries(util.join { options.path, queries }, queryPath) then
 				core.log('[Evergreen] Finished installing queries for ' .. options.lang)
-				if options.extensions ~= nil then
-					for ext in options.extensions:gmatch('[^,]+') do
-						M.extensionMappings[ext] = options.lang
-					end
-				end
-				if options.filename ~= nil then
-					M.filenameMappings[options.filename] = options.lang
-				end
-				return true
+				mapGrammar(options)
 			end
-		else
-			return false
+			return true;
 		end
 	else
 		core.error(
@@ -85,15 +103,10 @@ local function installGrammar(options)
 			options.lang,
 			options.path
 		)
-		return false
 	end
+	return false
 end
 
-local function rmDir(path)
-	exec(PLATFORM == 'Windows' and
-		{ 'cmd', '/c', 'rmdir ' .. path } or
-		{ 'sh', '-c', 'rm -rf ' .. path })
-end
 
 local function installGrammarFromGit(options)
 	local tmp_path = util.join { config.dataDir, "temp" }
@@ -104,12 +117,7 @@ local function installGrammarFromGit(options)
 		return true
 	end
 	core.log('[Evergreen] cloning ' .. options.git .. ' in ' .. repo_path)
-	-- local gitOut, gitCode =
 	exec({ "git", "clone", options.git, options.lang }, { cwd = tmp_path })
-	-- if gitCode ~= 0 then
-	-- core.error('[Evergreen] impossible to clone ' .. options.git .. ': '.. gitOut)
-	-- return false
-	-- end
 	if options.rev ~= nil then
 		core.log('[Evergreen] checkout revision ' .. options.rev)
 		exec({ "git", "checkout", options.rev }, { cwd = repo_path })
@@ -128,36 +136,28 @@ local function installGrammarFromGit(options)
 			if options.queries ~= nil then
 				queries = options.queries
 			end
-			local out, exitCode = exec(PLATFORM == 'Windows' and
-				{ 'cmd', '/c', 'cp ' .. util.join { queries, '*.scm' } .. ' ' .. queryPath } or
-				{ 'sh', '-c', 'cp ' .. util.join { queries, '*.scm' } .. ' ' .. queryPath }, { cwd = repo_path })
-			if exitCode ~= 0 then
-				core.error('[Evergreen] An error occured while copying ' .. options.lang .. 'queries \n' .. out)
-			else
+			if copyQueries(util.join { repo_path, queries }, queryPath) then
 				core.log('[Evergreen] Finished installing queries for ' .. options.lang)
-				if options.extensions ~= nil then
-					for ext in options.extensions:gmatch('[^,]+') do
-						M.extensionMappings[ext] = options.lang
-					end
-				end
-				if options.filename ~= nil then
-					M.filenameMappings[options.filename] = options.lang
-				end
+				mapGrammar(options)
 			end
+			rmDir(repo_path)
+			return true
+		else
+			rmDir(repo_path)
+			return false
 		end
-		-- rmDir(repo_path)
 	else
 		core.error(
 			"[Evergreen] impossible to install '%s' grammar as '%s' path does not exists.",
 			options.lang,
 			options.git
 		)
-		return false
 	end
 	return false
 end
 
-function M.add_grammar(options)
+
+function M.addGrammar(options)
 	local required_fields = {
 		"lang",
 	}
@@ -185,14 +185,7 @@ function M.add_grammar(options)
 			core.error("[Evergreen] nor path or git defined for " .. options.lang)
 		end
 	else
-		if options.extensions ~= nil then
-			for ext in options.extensions:gmatch('[^,]+') do
-				M.extensionMappings[ext] = options.lang
-			end
-		end
-		if options.filename ~= nil then
-			M.filenameMappings[options.filename] = options.lang
-		end
+		mapGrammar(options)
 	end
 
 	return true
